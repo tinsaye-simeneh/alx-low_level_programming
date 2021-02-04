a2edb503a36c274ad605e7c40bdaab3471760495
tinsaye1945 <64516393+tinsaye1945@users.noreply.github.com>
2021-02-04 17:31:59 +0300

    Create 4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int m;
+
+for (m = 97; m <= 100; m++)
+{
+putchar(m);
+}
+for (m = 102; m <= 112; m++)
+{
+putchar(m);
+}
+for (m = 114; m <= 122; m++)
+{
+putchar(m);
+}
+putchar('\n');
+return (0);
+}
