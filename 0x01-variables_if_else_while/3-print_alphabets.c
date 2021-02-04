484d7d85c042673bcacab4797400b1bfd8394dc1
tinsaye1945 <64516393+tinsaye1945@users.noreply.github.com>
2021-02-04 16:36:48 +0300

    Create 3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int n;
+n = 97;
+m = 65;
+while (n <= 122 )
+{
+putchar(n);
+n++;
+}
+while (m <= 90 )
+{
+putchar(m);
+n++;
+}
+putchar('\n');
+return (0);
+}
