c97818fc4c487990b4c35d5735cbebeca62b9fa3
tinsaye1945 <64516393+tinsaye1945@users.noreply.github.com>
2021-02-09 14:00:00 +0300

    Update 5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,31 +1,26 @@
 #include "holberton.h"
-
-int _putchar(char c);
-
 /**
- * print_sign - check the code for Holberton School students.
- *
- * @n: is an integer argument
- *
- * Return: Always 0.
+ * print_sign - checks if letter provided is in fact a letter
+ * @n: integer from which to check whether is positive, negative or zero
+ * Return: Void.
  */
 
 int print_sign(int n)
 {
-  if (n > 0)
-    {
-      _putchar('+');
-      return (1);
-    }
-  else if (n == 0)
-    {
-      _putchar('0');
-      return (0);
-    }
-  else if (n < 0)
-    {
-      _putchar('-');
-      return (1);
-    }
-  return (0);
+
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (1);
+	}
+	else
+	{
+		_putchar('0');
+		return (0);
+	}
 }
