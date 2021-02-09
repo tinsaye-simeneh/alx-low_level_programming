7eaf1e07f92bf615aa3730634736475e6223cc69
tinsaye1945 <64516393+tinsaye1945@users.noreply.github.com>
2021-02-09 13:52:18 +0300

    Update 4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -20,14 +20,4 @@ int _isalpha(int c)
 		return (1);
 	}
 	return (0);
-}#include "holberton.h"
-
-int _isalpha(int c)
-{
-	if (c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z')
-	{
-		return 1;
-	}
-	else 
-	return 0;
 }
