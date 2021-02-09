91fb153ec7b9e962cf3436b77c0835f28dc58dc3
tinsaye1945 <64516393+tinsaye1945@users.noreply.github.com>
2021-02-09 13:45:49 +0300

    is lower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,15 +1,18 @@
 #include "holberton.h"
+
 /**
- * _islower - func
+ * _islower - check the code for Holberton School students.
  *
- *@c: is a parameter
+ * @c:  is a parameter
  *
- * Return:  succes
+ * Return: Always 0.
  */
+
 int _islower(int c)
 {
-  if (c >= 'a' && c <= 'z')
-    return (1);
-  else
-    return (0);
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	return (0);
 }
