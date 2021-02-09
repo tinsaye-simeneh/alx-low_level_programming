966bc171027355518b15afc8af72211b6320c34b
tinsaye1945 <tinsayesimeneh608@gmail.com>
2021-02-09 10:44:25 +0000

    is lower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ * _islower - func
+ *
+ *@c: is a parameter
+ *
+ * Return:  succes
+ */
+int _islower(int c)
+{
+  if (c >= 'a' && c <= 'z')
+    return (1);
+  else
+    return (0);
+}
