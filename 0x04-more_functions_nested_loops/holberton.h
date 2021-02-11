c9c840933a91f3bbf1332762060ef158027d5bcc
tinsaye1945 <64516393+tinsaye1945@users.noreply.github.com>
2021-02-11 12:09:10 +0300

    Update holberton.h

--- 0x04-more_functions_nested_loops/holberton.h
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -11,4 +11,5 @@ extern void print_line(int n);
 extern void print_diagonal(int n);
 extern void print_square(int size);
 extern void print_triangle(int size);
+extern void print_number(int n);
 #endif
